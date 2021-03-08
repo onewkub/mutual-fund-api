047934343861212a3e2da746df20b9d672a0211b
Wachira Norasing <wachira_norasing@outlook.com>
2021-03-08 21:48:29 +0700

    Create Home Page for website

---
+++ server/client-app/src/pages/input/index.tsx
@@ -0,0 +1,13 @@
+import { Typography } from 'antd'
+
+const { Title } = Typography
+
+function Input() {
+  return (
+    <>
+      <Title>Input</Title>
+    </>
+  )
+}
+
+export default Input
---
+++ server/client-app/src/pages/result/index.tsx
@@ -0,0 +1,13 @@
+import { Typography } from 'antd'
+
+const { Title } = Typography
+
+function Result() {
+  return (
+    <>
+      <Title>Result</Title>
+    </>
+  )
+}
+
+export default Result
