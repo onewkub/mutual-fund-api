7c4171eba15917fbfeaa32aed5e7ea0f8cc03337
Wachira Norasing <wachira_norasing@outlook.com>
2021-03-11 22:56:48 +0700

    add Swagger ui and prisma

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,25 @@
 import express from 'express'
 import { Request, Response } from 'express'
+import 'dotenv/config'
 
 import swaggerUI from 'swagger-ui-express'
 import swaggerFile from './swagger'
+import router from './routers'
+
+const PORT = process.env.PORT || 8080
 
 const server = express()
-const PORT = 8080
+server.use(express.json())
 
 server.get('/', (req: Request, res: Response) => {
   res.send(
     'Hello This is server of My Project Yeah!! TypeScript + Express + Nodemon',
   )
 })
+server.use('/api', router)
 
-server.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerFile))
+server.use('/api/doc', swaggerUI.serve, swaggerUI.setup(swaggerFile))
 
 server.listen(PORT, () => {
-  console.log(`Server is running at https://localhost:${PORT}`)
+  console.log(`Server is running at http://localhost:${PORT}`)
 })
--- server/src/routers/index.ts
+++ server/src/routers/index.ts
@@ -1,7 +1,12 @@
 import { Router, Request, Response } from 'express'
 
 const router = Router()
-
+/** 
+ * @swagger
+ * /test:
+ *    get:
+ *      description: "Just test and swagger"
+ */
 router.get('/test', (req: Request, res: Response) => {
   // console.log(req)
   res.send("Hello I'm here for you!!")
