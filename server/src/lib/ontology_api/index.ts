80eecd31a8e115058adb5ca779f621bccc8ee6e7
Wachira Norasing <wachira_norasing@outlook.com>
2021-03-15 17:38:45 +0700

    Add init db script

--- server/src/lib/ontology_api/index.ts
+++ server/src/lib/ontology_api/index.ts
@@ -11,9 +11,128 @@ export const httpRequest_ontology = new HttpRequest(
 )
 
 export async function updateOntology(command: string) {
-  await httpRequest_ontology.post(`?update=${prefix} ${command}`)
+  const update: string = `${prefix} ${command}`
+  // console.log(path)
+  await httpRequest_ontology.post('/', null, {
+    params: {
+      update,
+    },
+  })
 }
 
 export async function queryOntology(command: String) {
   await httpRequest_ontology.post(`?query=${prefix} ${command}`)
 }
+
+export interface IFundInsert {
+  project_id: string
+  project_name: string
+  project_class_name: string
+  project_loss: number
+  project_risk: number
+  project_policy: string
+}
+
+export interface IAssetInsert {
+  asset_id: string
+  asset_name: string
+  asset_symbol: string
+}
+
+export async function addInvestRelation(project_id: string, asset_id: string) {
+  asset_id = asset_id.replace('&', '-').trim().replace('/t', '')
+  const command = `
+  INSERT{
+    ?fund mat:invest mat:${asset_id}
+  }
+  WHERE{
+    ?fund mat:project_id  "${project_id}"^^xsd:string
+  }
+  `
+  try {
+    await updateOntology(command)
+    console.log(`Insert invest from ${project_id} => ${asset_id}`)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export async function addAssetOntology(asset: IAssetInsert) {
+  asset = {
+    ...asset,
+    asset_id: asset.asset_id.replace('&', '-').trim().replace('/t', ''),
+  }
+  const command = `
+  INSERT DATA{
+    mat:${asset.asset_id} rdf:type mat:asset;
+    mat:asset_id "${asset.asset_id}"^^xsd:string;
+    mat:asset_name "${asset.asset_name}"^^xsd:string;
+    mat:asset_symbol "${asset.asset_symbol}"^^xsd:string.
+  }
+  `
+  try {
+    updateOntology(command)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+export async function addFundOntology(fund: IFundInsert) {
+  fund = {
+    ...fund,
+    project_name: fund.project_name.replace('&', '-').trim().replace('/t', ''),
+  }
+  let command = `
+  INSERT DATA { mat:${fund.project_name} rdf:type mat:fund;
+  rdf:type mat:${fund.project_policy};
+  mat:project_id "${fund.project_id}"^^xsd:string;
+  mat:project_name "${fund.project_name}"^^xsd:string;
+  mat:risk_rate "${fund.project_risk}"^^xsd:integer;
+  ${
+    fund.project_class_name !== '-'
+      ? `rdf:type mat:${getProjectClass(fund.project_class_name)};`
+      : ''
+  }
+  ${
+    fund.project_loss
+      ? `mat:project_loss "${fund.project_loss}"^^xsd:decimal .`
+      : ''
+  } 
+  }`
+  try {
+    console.log(`insert ${fund.project_name}`)
+    await updateOntology(command)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+function getProjectClass(className: string) {
+  switch (className) {
+    case 'ชนิดสะสมมูลค่า':
+      return 'accumurate'
+    case 'ชนิดจ่ายเงินปันผล':
+      return 'dividen'
+    default:
+      return 'accumurate'
+  }
+}
+
+const mock_data: IFundInsert = {
+  project_id: 'M0643_2555',
+  project_class_name: 'ชนิดสะสมมูลค่า',
+  project_loss: -27.592499999999998,
+  project_name: 'SCBS&P500A',
+  project_policy: 'equity_fund',
+  project_risk: 6,
+}
+
+function test() {
+  try {
+    addFundOntology(mock_data)
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// test()
